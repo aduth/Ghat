5023335e362e4a45518009bc0684d70289425fac
Andrew Duthie <aduthie7@gmail.com>
2014-12-21 14:57:18 -0500

    Add missing prop validation

--- client/components/steps.jsx
+++ client/components/steps.jsx
@@ -1,10 +1,15 @@
 var React = require( 'react' ),
+    TokenStore = require( '../stores/token' ),
     observe = require( '../mixins/observe-store' );
 
 module.exports = React.createClass({
     displayName: 'Steps',
 
     mixins: [ observe( 'tokens' ) ],
+
+    propTypes: {
+        tokens: React.PropTypes.instanceOf( TokenStore ).isRequired
+    },
 
     authenticate: function() {
         window.open( '/authorize/github' );
