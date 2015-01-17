a99e7c2174734d35d5fbc1f012ec81541153957b
Andrew Duthie <aduthie7@gmail.com>
2015-01-17 18:48:38 -0500

    Replace store with support for non-browser environments

--- client/stores/local.js
+++ client/stores/local.js
@@ -1,46 +1,47 @@
 var EventEmitter = require( 'events' ).EventEmitter,
-    store = require( 'store' ),
+    supportsLocalStorage = 'undefined' !== typeof window && window.localStorage,
     LocalStore;
 
 LocalStore = module.exports = function( name ) {
     this.name = name;
+
+    if ( supportsLocalStorage ) {
+        try {
+            this.store = JSON.parse( window.localStorage.getItem( name ) );
+        } catch ( e ) {}
+    }
+    this.store = this.store || {};
 };
 
 LocalStore.prototype = Object.create( EventEmitter.prototype );
 
 LocalStore.prototype.getAll = function() {
-    return store.get( this.name );
+    return this.store;
 };
 
 LocalStore.prototype.get = function( key ) {
-    var data = store.get( this.name );
-
-    if ( data ) {
-        return data[ key ];
-    }
+    return this.store[ key ];
 };
 
 LocalStore.prototype.set = function( key, value ) {
-    var data = store.get( this.name );
+    this.store[ key ] = value;
 
-    if ( ! data ) {
-        data = {};
+    if ( supportsLocalStorage ) {
+        window.localStorage.setItem( this.name, JSON.stringify( this.store ) );
     }
 
-    data[ key ] = value;
-    store.set( this.name, data );
     this.emit( 'change' );
 };
 
 LocalStore.prototype.remove = function( key ) {
-    var data = store.get( this.name );
+    delete this.store[ key ];
 
-    delete data[ key ];
-
-    if ( Object.keys( data ).length ) {
-        store.set( this.name, data );
-    } else {
-        store.remove( this.name );
+    if ( supportsLocalStorage ) {
+        if ( Object.keys( this.store ).length ) {
+            window.localStorage.setItem( this.name, JSON.stringify( this.store ) );
+        } else {
+            window.localStorage.removeItem( this.name );
+        }
     }
 
     this.emit( 'change' );
