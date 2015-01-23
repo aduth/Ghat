2d67b7b280d72ede189f6d3e6685e90b771f0da8
Andrew Duthie <aduthie7@gmail.com>
2015-01-22 19:55:32 -0500

    Log error stack
    In place of nonexistent fileName and lineNumber

--- server/responses/postmessage.js
+++ server/responses/postmessage.js
@@ -18,7 +18,8 @@ module.exports.success = function( req, res, next ) {
 
 module.exports.failure = function( err, req, res, next ) {
     if ( ! ( err instanceof errors.Base ) ) {
-        helpers.log.error( 'An unknown error occurred: %s (%s:%d)', err.message, err.fileName, err.lineNumber );
+        helpers.log.error( 'An unknown error occurred: %s', err.message );
+        helpers.log.error( err.stack );
         err = new errors.Base();
     }
 
