50aa90437a02a9c63bd59d4c6d164ccb294582bb
Andrew Duthie <aduthie7@gmail.com>
2015-01-23 21:20:14 -0500

    Enhance field extraction to support array plucking

--- server/helpers/field.js
+++ server/helpers/field.js
@@ -1,9 +1,13 @@
+var pluck = require( 'lodash-node/modern/collections/pluck' );
+
 module.exports.getValue = function( object, field ) {
     var parts = field.split( '.' );
 
     return parts.reduce(function( memo, part ) {
         if ( 'object' === typeof memo && part in memo ) {
             return memo[ part ];
+        } else if ( Array.isArray( memo ) ) {
+            return pluck( memo, part );
         }
     }, object );
 };
