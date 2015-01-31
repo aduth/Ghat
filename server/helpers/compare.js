d493b627586671c05b56d2bba9f3446726ec4528
Andrew Duthie <andrew@andrewduthie.com>
2015-01-31 10:18:03 -0500

    Use loose comparison for equals

--- server/helpers/compare.js
+++ server/helpers/compare.js
@@ -1,12 +1,12 @@
 module.exports.isMatch = function( source, operator, target ) {
     switch ( operator ) {
-        case '!=': return source !== target;
+        case '!=': return source != target; /* jshint ignore:line */
         case '<': return source < target;
         case '<=': return source <= target;
         case '>=': return source >= target;
         case '>': return source > target;
         case 'in': return ( Array.isArray( target ) || 'string' === typeof target ) && -1 !== target.indexOf( source );
         case 'contains': return ( Array.isArray( source ) || 'string' === typeof source ) && -1 !== source.indexOf( target );
-        default: return source === target;
+        default: return source == target; /* jshint ignore:line */
     }
 };
