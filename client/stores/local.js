63b750ec9d719057b654988707d5120b0176dd0f
Andrew Duthie <andrew@andrewduthie.com>
2015-02-01 14:23:32 -0500

    Short-circuit default to ObjectStore if localStorage not supported

--- client/stores/local.js
+++ client/stores/local.js
@@ -1,37 +1,35 @@
 var ObjectStore = require( './object' ),
-    supportsLocalStorage = 'undefined' !== typeof window && window.localStorage,
     LocalStore;
+
+if ( 'undefined' === typeof window || window.localStorage ) {
+    module.exports = ObjectStore;
+    return;
+}
 
 LocalStore = module.exports = function( name ) {
     ObjectStore.call( this );
     this.name = name;
 
-    if ( supportsLocalStorage ) {
-        try {
-            this.store = JSON.parse( window.localStorage.getItem( name ) );
-        } catch ( e ) {}
+    try {
+        this.store = JSON.parse( window.localStorage.getItem( name ) );
+    } catch ( e ) {
+        this.store = {};
     }
-    this.store = this.store || {};
 };
 
 LocalStore.prototype = Object.create( ObjectStore.prototype );
 
 LocalStore.prototype.set = function( key, value ) {
     ObjectStore.prototype.set.call( this, key, value );
-
-    if ( supportsLocalStorage ) {
-        window.localStorage.setItem( this.name, JSON.stringify( this.store ) );
-    }
+    window.localStorage.setItem( this.name, JSON.stringify( this.store ) );
 };
 
 LocalStore.prototype.remove = function( key ) {
     ObjectStore.prototype.remove.call( this, key );
 
-    if ( supportsLocalStorage ) {
-        if ( Object.keys( this.store ).length ) {
-            window.localStorage.setItem( this.name, JSON.stringify( this.store ) );
-        } else {
-            window.localStorage.removeItem( this.name );
-        }
+    if ( Object.keys( this.store ).length ) {
+        window.localStorage.setItem( this.name, JSON.stringify( this.store ) );
+    } else {
+        window.localStorage.removeItem( this.name );
     }
 };
