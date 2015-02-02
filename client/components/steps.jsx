3f2954e38fc9affc27b7f1d79d65584f5eeee4e7
Andrew Duthie <aduthie7@gmail.com>
2015-02-01 21:23:12 -0500

    Clarify GitHub token usage

--- client/components/steps.jsx
+++ client/components/steps.jsx
@@ -29,7 +29,7 @@ module.exports = React.createClass({
                     tokens={ this.props.tokens }
                     profiles={ this.props.profiles }
                     title="Connect to GitHub"
-                    description="To create the webhooks necessary to relay events to your chat client, you must authorize Ghat to access your GitHub account."
+                    description="To create the webhooks necessary to relay events to your chat client, you must authorize Ghat to access your GitHub account. GitHub tokens are never saved to Ghat's servers."
                     showSelect={ false } />
                 <Connection
                     name="chat"
