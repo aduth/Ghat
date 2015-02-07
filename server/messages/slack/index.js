65fdd83e55b2e924d783a0c6f8835477c8289636
Andrew Duthie <aduthie7@gmail.com>
2015-02-06 23:31:32 -0500

    Add pull request review comment Slack message

--- server/messages/slack/index.js
+++ server/messages/slack/index.js
@@ -3,5 +3,6 @@ module.exports = {
     issue_comment: require( './issue-comment' ),
     issues: require( './issues' ),
     pull_request: require( './pull-request' ),
+    pull_request_review_comment: require( './pull-request-review-comment' ),
     push: require( './push' )
 };
---
+++ server/messages/slack/pull-request-review-comment.js
@@ -0,0 +1,13 @@
+var format = require( 'util' ).format,
+    constants = require( '../../../shared/constants/' );
+
+module.exports = function( body ) {
+    return {
+        fallback: format( '[%s] Pull request commented on by %s - #%d %s', body.repository.full_name, body.sender.login, body.pull_request.number, body.pull_request.title ),
+        pretext: format( '[%s] Pull request commented on by %s', body.repository.full_name, body.sender.login ),
+        title: format( '#%d %s', body.pull_request.number, body.pull_request.title ),
+        title_link: body.pull_request.html_url,
+        text: body.comment.body,
+        color: constants.app.COLOR_SECONDARY
+    };
+};
