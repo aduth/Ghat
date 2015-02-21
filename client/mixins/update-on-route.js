03a8c6efba0bae288d2d0bd16743ca2a90fef809
Andrew Duthie <aduthie7@gmail.com>
2015-02-20 22:41:29 -0500

    Fix broken router caused by typo

--- client/mixins/update-on-route.js
+++ client/mixins/update-on-route.js
@@ -1,7 +1,7 @@
 var assign = require( 'lodash/object/assign' ),
     eventMonitor = require( './event-monitor' );
 
-module.exports = assign({}, eventMonitor( 'route', 'route', 'forceUpdateOnRoute' ), {
+module.exports = assign({}, eventMonitor( 'router', 'route', 'forceUpdateOnRoute' ), {
     forceUpdateOnRoute: function() {
         if ( this.isMounted() ) {
             this.forceUpdate();
