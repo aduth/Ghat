679c4eebeabe92b393348c0df2e1399b2b7c7579
Andrew Duthie <aduthie7@gmail.com>
2015-02-20 21:30:49 -0500

    Rename mixin force update message
    To be more specific

--- client/mixins/observe-store.js
+++ client/mixins/observe-store.js
@@ -2,8 +2,8 @@ var assign = require( 'lodash/object/assign' ),
     eventMonitor = require( './event-monitor' );
 
 module.exports = function( stores ) {
-    return assign({}, eventMonitor( stores, 'change', 'forceUpdateIfMounted' ), {
-        forceUpdateIfMounted: function() {
+    return assign({}, eventMonitor( stores, 'change', 'forceUpdateOnStoreChange' ), {
+        forceUpdateOnStoreChange: function() {
             if ( this.isMounted() ) {
                 this.forceUpdate();
             }
