06e74f4559e99c4933a2c2101ef82fd3b91b8a49
Andrew Duthie <aduthie7@gmail.com>
2015-02-26 22:07:09 -0500

    Add Slack fields for issue label and assigning

--- server/messages/slack/issues.js
+++ server/messages/slack/issues.js
@@ -2,7 +2,7 @@ var format = require( 'util' ).format,
     constants = require( '../../../shared/constants/' );
 
 module.exports = function( body ) {
-    return {
+    var message = {
         fallback: format( '[%s] Issue %s by %s - #%d: %s', body.repository.full_name, body.action, body.sender.login, body.issue.number, body.issue.title ),
         pretext: format( '[%s] Issue %s by %s', body.repository.full_name, body.action, body.sender.login ),
         title: format( '#%d: %s', body.issue.number, body.issue.title ),
@@ -10,4 +10,25 @@ module.exports = function( body ) {
         text: body.issue.body,
         color: constants.app.COLOR_SECONDARY
     };
+
+    switch ( body.action ) {
+        case 'labeled':
+        case 'unlabeled':
+            message.fields = [{
+                title: 'Label ' + ( 'labeled' === body.action ? 'added' : 'removed' ),
+                value: body.label.name,
+                short: true
+            }];
+            break;
+        case 'assigned':
+        case 'unassigned':
+            message.fields = [{
+                title: 'User ' + ( 'assigned' === body.action ? 'assigned' : 'unassigned' ),
+                value: body.assignee.login,
+                short: true
+            }];
+            break;
+    }
+
+    return message;
 };
--- test/specs/server/messages/slack/issues.js
+++ test/specs/server/messages/slack/issues.js
@@ -11,5 +11,40 @@ describe( 'issues', function() {
         expect( message.title ).to.equal( '#51: Spelling error in the README file' );
         expect( message.title_link ).to.equal( 'https://github.com/baxterthehacker/public-repo/issues/51' );
         expect( message.text ).to.equal( 'It looks like you accidently spelled \'commit\' with two \'t\'s.' );
+        expect( message.fields ).to.eql([{
+            title: 'Label added',
+            value: 'bug',
+            short: true
+        }]);
+    });
+
+    it( 'should omit fields if not assigned or labeled', function() {
+        var copy = JSON.parse( JSON.stringify( payload ) ),
+            message;
+
+        copy.action = 'opened';
+        message = generateMessage( copy );
+
+        expect( message.fields ).to.be.undefined;
+    });
+
+    it( 'should generate the expected message for assigned action', function() {
+        var copy = JSON.parse( JSON.stringify( payload ) ),
+            message;
+
+        copy.action = 'assigned';
+        copy.assignee = { login: 'aduth' };
+        message = generateMessage( copy );
+
+        expect( message.fallback ).to.equal( '[baxterthehacker/public-repo] Issue assigned by baxterthehacker - #51: Spelling error in the README file' );
+        expect( message.pretext ).to.equal( '[baxterthehacker/public-repo] Issue assigned by baxterthehacker' );
+        expect( message.title ).to.equal( '#51: Spelling error in the README file' );
+        expect( message.title_link ).to.equal( 'https://github.com/baxterthehacker/public-repo/issues/51' );
+        expect( message.text ).to.equal( 'It looks like you accidently spelled \'commit\' with two \'t\'s.' );
+        expect( message.fields ).to.eql([{
+            title: 'User assigned',
+            value: 'aduth',
+            short: true
+        }]);
     });
 });
