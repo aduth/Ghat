7397ddc632b2ead07615a84115c1a8888cea3239
Andrew Duthie <aduthie7@gmail.com>
2015-04-25 15:51:03 -0400

    Prevent excessive pull request messages

--- server/messages/slack/pull-request.js
+++ server/messages/slack/pull-request.js
@@ -2,6 +2,15 @@ var format = require( 'util' ).format,
     constants = require( '../../../shared/constants/' );
 
 module.exports = function( body ) {
+    var timeSinceCreated;
+
+    if ( 'opened' !== body.action ) {
+        timeSinceCreated = Date.now() - Date.parse( body.pull_request.created_at );
+        if ( timeSinceCreated < constants.app.ISSUE_IGNORE_CREATED_WITHIN_MS ) {
+            return;
+        }
+    }
+
     return {
         fallback: format( '[%s] Pull request %s by %s - #%d: %s', body.repository.full_name, body.action, body.sender.login, body.pull_request.number, body.pull_request.title ),
         pretext: format( '[%s] Pull request %s by %s', body.repository.full_name, body.action, body.sender.login ),
